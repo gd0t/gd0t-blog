80d2ac126dd2839f74850a8cdc76d0f92ffd2094
rv00349 <rv00349@surrey.ac.uk>
2026-05-17 21:49:05 +0100

    Adding block to git

---
+++ gd0t/src/test/java/com/gd0t/gd0t/Gd0tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd0t.gd0t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gd0tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
